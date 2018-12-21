055071387dc3ff5b283c20fd83ea2db0bab11b7d
alineuwi <alikimy02@gmail.com>
2018-12-21 16:32:35 +0200

    js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,23 +1,51 @@
 function check(){
 
-  var q1 = Music.quiz.q1.value;
-  var q2 = Music.quiz.q2.value;
-  var q3 = Music.quiz.q3.value;
-  var q4 = Music.quiz.q4.value;
+  var questionOne = document.quiz.questionOne.value;
+  var questionTwo = document.quiz.questionTwo.value;
+  var questionThree = document.questionThree.value;
+  var questionFour = document.quiz.questionFour.value;
+  var questionFive = document.quiz.questionFive.value;
+  var questionSix = document.quiz.questionSix.value;
+  var questionSeven = document. questionSeven.value;
+  var questionEight = document.quiz.questionEight.value;
+  var questionNine = document. questionNine.value;
+  var questionTen = document.quiz. questionTen.value;
   var count=0;
 
-      if (q1 == "c") {        
-        count+=25;
+      if (Question1 == "window.print"){      
+        count+=2;
       }
-      if (q2 == "c"){
-        count+=25;
+      if (Question2 == "forEach"){
+        count+=2;
       }
-      if (q3 == "b"){
-        count+=25;
+      if (Question3 == "toUpperCase"){
+        count+=2;
       }
-      if (q4 == "c"){
-        count+=25;
+      if (Question4 == "c"){
+        count+=2;
       }
+      if (Question5 == "c"){
+        count+=2;
+      }
+      if (Question6 == "c"){
+        count+=2;
+      }
+      if (Question7 == "c"){
+        count+=2;
+      }
+      if (Question8 == "c"){
+        count+=2;
+      }
+      if (Question9 == "c"){
+        count+=2;
+      }
+      if (Question10 == "c"){
+        count+=2;
+      }
+      if (Question11 == "c"){
+        count+=2;
+      }
+
   document.getElementById("after_submit").style.visibility ="visible";
 
   document.getElementById("number_count").innerHTML = "You got " +count + "%.";
